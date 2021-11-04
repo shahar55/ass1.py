e0ff617c753c203740afc3014fd16380d591f652
shahar <sasa0192837465@gmail.com>
2021-11-04 17:32:22 +0200

    finish client

--- client.py
+++ client.py
@@ -1,7 +1,30 @@
 import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'id: 214453821, name: shahar', ('127.0.0.1', 12345))
-data, addr = s.recvfrom(1024)
-print(str(data), addr)
-s.close()
+import sys
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ipNum = sys.argv[1]
+    portNum = sys.argv[2]
+    bufferSize = 96
+    sendingCounter = 1
+    flag = True
+    with open(sys.argv[3], "rb") as in_file:
+        while True:
+            if flag:
+                piece = in_file.read(bufferSize)
+                if piece == "":
+                    break  # end of file
+                s.sendto((str(sendingCounter) + "." + piece).encode(), (ipNum, portNum))
+            s.settimeout(5)
+            try:
+                data, address = s.recvfrom(100)
+            except socket.timeout:
+                flag = False
+                continue
+            if data.decode("utf-8") != piece:
+                flag = False
+                continue
+            flag = True
+            sendingCounter = sendingCounter + 1
+    s.close()
+if __name__ == '__main__':
+     main()
