d72a22e23a78ee24abec1989a924c825ecf9dae4
ronenius <ronenius4@gmail.com>
2021-11-04 16:35:28 +0200

    implemented server

--- server.py
+++ server.py
@@ -1,7 +1,16 @@
 import socket
+import sys
+
+MY_PORT = int(sys.argv[1])
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 12345))
+s.bind(('', MY_PORT))
+packetCounter = 0
 while True:
-    data, addr = s.recvfrom(1024)
-    print(str(data), addr)
-    s.sendto(data.upper(), addr)
+    data, addr = s.recvfrom(100)
+    text = str(data)
+    numPacket, text = text.split('.', 1)
+    if numPacket == packetCounter + 1:
+        print(text)
+        packetCounter = packetCounter + 1
+    s.sendto(data, addr)
