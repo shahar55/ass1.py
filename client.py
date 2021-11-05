5f0c8827b6013a8693c37399ba5212e8a8454afd
ronenius <ronenius4@gmail.com>
2021-11-05 04:21:54 +0200

    debugging

--- client.py
+++ client.py
@@ -1,30 +1,35 @@
 import socket
 import sys
+
+
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ipNum = sys.argv[1]
-    portNum = sys.argv[2]
-    bufferSize = 96
+    portNum = int(sys.argv[2])
+    bufferSize = 100
     sendingCounter = 1
     flag = True
-    with open(sys.argv[3], "rb") as in_file:
+    with open(sys.argv[3], "r") as in_file:
         while True:
             if flag:
-                piece = in_file.read(bufferSize)
+                piece = in_file.read(bufferSize - 4)
                 if piece == "":
                     break  # end of file
-                s.sendto((str(sendingCounter) + "." + piece).encode(), (ipNum, portNum))
-            s.settimeout(5)
+            s.sendto((str(sendingCounter) + "." +
+                     piece).encode(), (ipNum, portNum))
+            s.settimeout(1)
             try:
-                data, address = s.recvfrom(100)
+                data, address = s.recvfrom(bufferSize)
             except socket.timeout:
                 flag = False
                 continue
-            if data.decode("utf-8") != piece:
+            if data != (str(sendingCounter) + "." + piece).encode():
                 flag = False
                 continue
             flag = True
             sendingCounter = sendingCounter + 1
     s.close()
+
+
 if __name__ == '__main__':
-     main()
+    main()
--- server.py
+++ server.py
@@ -1,16 +1,25 @@
 import socket
 import sys
 
-MY_PORT = int(sys.argv[1])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', MY_PORT))
-packetCounter = 0
-while True:
-    data, addr = s.recvfrom(100)
-    text = str(data.decode())
-    numPacket, text = text.split('.', 1)
-    if int(numPacket) == packetCounter + 1:
-        print(text, addr)
-        packetCounter = packetCounter + 1
-    s.sendto(data, addr)
+def main():
+    MY_PORT = int(sys.argv[1])
+    BUFFER_SIZE = 100
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', MY_PORT))
+    clientCounterDict = {}
+    while True:
+        data, addr = s.recvfrom(BUFFER_SIZE)
+        if not addr in clientCounterDict.keys():
+            clientCounterDict[addr] = 0
+        text = str(data.decode())
+        numPacket, text = text.split('.', 1)
+        if int(numPacket) == clientCounterDict[addr] + 1:
+            print(text, end='')
+            clientCounterDict[addr] = clientCounterDict[addr] + 1
+        s.sendto(data, addr)
+
+
+if __name__ == '__main__':
+    main()
